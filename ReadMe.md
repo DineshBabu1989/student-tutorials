b0f65822cc2a4f68d0ce8dc9723330d9da33a690
Dinesh Babu <emailerdinesh@gmail.com>
2024-02-12 20:47:47 +0530

    basic navbar

---
+++ ReadMe.md
@@ -0,0 +1,14 @@
+## React Basics
+
+- Setting up a project
+
+  ```
+  npm create vite@latest basics -- --template react-ts
+
+  ```
+
+- Setting up tailwind
+
+```
+https://tailwindcss.com/docs/guides/vite
+```
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,10 @@
+export default function Navbar() {
+  return (
+    <nav>
+      <div className="w-full flex items-center justify-between p-3 bg-black">
+        <span>Logo</span>
+        <button>x</button>
+      </div>
+    </nav>
+  );
+}
